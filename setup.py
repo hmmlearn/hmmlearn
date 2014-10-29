fd2507d3557915197004334d4dd11664c914bb1c
Sergei Lebedev <superbobry@gmail.com>
2014-10-29 22:51:34 +0300

    MAINT simplified `setup.py`
      Unlike the version inherited from `scikit-learn` the new version
      requires Cython to be installed

--- setup.py
+++ setup.py
@@ -7,133 +7,66 @@
 descr = """Hidden Markov Models in Python with scikit-learn like API"""
 
 import sys
-import os
-import shutil
-from distutils.command.clean import clean as Clean
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 
-DISTNAME = 'hmmlearn'
-DESCRIPTION = 'A set of python modules for machine learning and data mining'
-LONG_DESCRIPTION = open('README.rst').read()
-MAINTAINER = 'orphaned'
-MAINTAINER_EMAIL = 'anonymous@world.org'
-#URL = 'http://scikit-learn.org'
-LICENSE = 'new BSD'
-#DOWNLOAD_URL = 'http://sourceforge.net/projects/scikit-learn/files/'
+DISTNAME = "hmmlearn"
+DESCRIPTION = __doc__
+LONG_DESCRIPTION = open("README.rst").read()
+MAINTAINER = "Sergei Lebedev"
+MAINTAINER_EMAIL = "superbobry@gmail.com"
+LICENSE = "new BSD"
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved",
+    "Programming Language :: C",
+    "Programming Language :: Python",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Topic :: Software Development",
+    "Topic :: Scientific/Engineering",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+]
 
 import hmmlearn
 
 VERSION = hmmlearn.__version__
 
-###############################################################################
-# Optional setuptools features
-# We need to import setuptools early, if we want setuptools features,
-# as it monkey-patches the 'setup' function
 
-# For some commands, use setuptools
-if len(set(('develop', 'release', 'bdist_egg', 'bdist_rpm',
-           'bdist_wininst', 'install_egg_info', 'build_sphinx',
-           'egg_info', 'easy_install', 'upload',
-           '--single-version-externally-managed',
-            )).intersection(sys.argv)) > 0:
-    import setuptools
-    extra_setuptools_args = dict(
-        zip_safe=False,  # the package can run out of an .egg file
-        include_package_data=True,
-    )
-else:
-    extra_setuptools_args = dict()
-
-###############################################################################
-
-class CleanCommand(Clean):
-    description = "Remove build directories, and compiled file in the source tree"
-
-    def run(self):
-        Clean.run(self)
-        if os.path.exists('build'):
-            shutil.rmtree('build')
-        for dirpath, dirnames, filenames in os.walk('hmmlearn'):
-            for filename in filenames:
-                if (filename.endswith('.so') or filename.endswith('.pyd')
-                             or filename.endswith('.dll')
-                             or filename.endswith('.pyc')):
-                    os.unlink(os.path.join(dirpath, filename))
+setup_options = dict(
+    name="hmmlearn",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    license=LICENSE,
+    url="https://github.com/hmmlearn/hmmlearn",
+    packages=["hmmlearn", "hmmlearn.utils"],
+    classifiers=CLASSIFIERS,
+    cmdclass={"build_ext": build_ext},
+    ext_modules=[
+        Extension("hmmlearn._hmmc", ["hmmlearn/_hmmc.pyx"])
+    ],
+    requires=["sklearn"]
+)
 
 
+# For these actions, NumPy is not required. We want them to succeed without,
+# for example when pip is used to install seqlearn without NumPy present.
+NO_NUMPY_ACTIONS = ('--help-commands', 'egg_info', '--version', 'clean')
+if not ('--help' in sys.argv[1:]
+        or len(sys.argv) > 1 and sys.argv[1] in NO_NUMPY_ACTIONS):
+    import numpy as np
+    setup_options['include_dirs'] = [np.get_include()]
 
-###############################################################################
-def configuration(parent_package='', top_path=None):
-    if os.path.exists('MANIFEST'):
-        os.remove('MANIFEST')
-
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None, parent_package, top_path)
-
-    # Avoid non-useful msg:
-    # "Ignoring attempt to set 'name' (from ... "
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-
-    config.add_subpackage('hmmlearn')
-
-    return config
-
-
-def setup_package():
-    metadata = dict(name=DISTNAME,
-                    maintainer=MAINTAINER,
-                    maintainer_email=MAINTAINER_EMAIL,
-                    description=DESCRIPTION,
-                    license=LICENSE,
-                    #url=URL,
-                    version=VERSION,
-                    #download_url=DOWNLOAD_URL,
-                    long_description=LONG_DESCRIPTION,
-                    classifiers=['Intended Audience :: Science/Research',
-                                 'Intended Audience :: Developers',
-                                 'License :: OSI Approved',
-                                 'Programming Language :: C',
-                                 'Programming Language :: Python',
-                                 'Topic :: Software Development',
-                                 'Topic :: Scientific/Engineering',
-                                 'Operating System :: Microsoft :: Windows',
-                                 'Operating System :: POSIX',
-                                 'Operating System :: Unix',
-                                 'Operating System :: MacOS',
-                                 'Programming Language :: Python :: 2',
-                                 'Programming Language :: Python :: 2.6',
-                                 'Programming Language :: Python :: 2.7',
-                                 'Programming Language :: Python :: 3',
-                                 'Programming Language :: Python :: 3.3',
-                                 ],
-                    cmdclass={'clean': CleanCommand},
-                    **extra_setuptools_args)
-
-    if (len(sys.argv) >= 2
-            and ('--help' in sys.argv[1:] or sys.argv[1]
-                 in ('--help-commands', 'egg_info', '--version', 'clean'))):
-
-        # For these actions, NumPy is not required.
-        #
-        # They are required to succeed without Numpy for example when
-        # pip is used to install Scikit when Numpy is not yet present in
-        # the system.
-        try:
-            from setuptools import setup
-        except ImportError:
-            from distutils.core import setup
-
-        metadata['version'] = VERSION
-    else:
-        from numpy.distutils.core import setup
-
-        metadata['configuration'] = configuration
-
-    setup(**metadata)
-
-
-if __name__ == "__main__":
-    setup_package()
+setup(**setup_options)
