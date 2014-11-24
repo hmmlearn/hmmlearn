cc1bb69becae6f37e826754071daa09483e96ae9
csytracy <csytracy@gmail.com>
2014-11-24 13:54:15 -0500

    format change according to sklearn.

--- hmmlearn/hmm.py
+++ hmmlearn/hmm.py
@@ -773,7 +773,7 @@ class GaussianHMM(_BaseHMM):
                 cv.shape = (1, 1)
             self._covars_ = distribute_covar_matrix_to_match_covariance_type(
                 cv, self._covariance_type, self.n_components)
-            self._covars_[self._covars_==0] = 1e-5
+            self._covars_[self._covars_ == 0] = 1e-5
 
     def _initialize_sufficient_statistics(self):
         stats = super(GaussianHMM, self)._initialize_sufficient_statistics()
