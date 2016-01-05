BUG fixed yet another normalization bug in ``_BaseHMM``
  The +epsilon hack in in ``hmmlearn.utils.normalize`` resulted
  in unnormalized start/transition probabilities. I think we can
  safely remove the +epsilon because ``normalize`` is never called
  on all-zero matrices.

  See #77 on GitHub for details

hmmlearn/tests/test_gaussian_hmm.py
@@ -207,10 +207,8 @@ class TestGaussianHMMWithDiagonalCovars(GaussianHMMTestMixin, TestCase):
         h.transmat_ = transmat.copy()
         h.fit(X)
 
-        assert np.allclose(startprob[startprob == 0.0],
-                           h.startprob_[startprob == 0.0])
-        assert np.allclose(transmat[transmat == 0.0],
-                           h.transmat_[transmat == 0.0])
+        assert (h.startprob_[startprob == 0.0] == 0.0).all()
+        assert (h.transmat_[transmat == 0.0] == 0.0).all()
 
         posteriors = h.predict_proba(X)
         assert not np.isnan(posteriors).any()

hmmlearn/tests/test_utils.py
@@ -4,14 +4,18 @@ from hmmlearn.utils import normalize, logsumexp
 
 
 def test_normalize():
-    A = np.random.normal((128, 4), 42.)
+    A = np.random.normal(42., size=128)
+    A[np.random.choice(len(A), size=16)] = 0.0
+    assert (A == 0.0).any()
     normalize(A)
     assert np.allclose(A.sum(), 1.)
 
 
 def test_normalize_along_axis():
-    A = np.random.normal((128, 4), 42.)
+    A = np.random.normal(42., size=(128, 4))
     for axis in range(A.ndim):
+        A[np.random.choice(len(A), size=16), axis] = 0.0
+        assert (A[:, axis] == 0.0).any()
         normalize(A, axis=axis)
         assert np.allclose(A.sum(axis=axis), 1.)
 

hmmlearn/utils.py
@@ -7,7 +7,7 @@ def normalize(a, axis=None):
 
     Parameters
     ----------
-    a : array_like
+    a : array
         Non-normalized input data.
 
     axis : int
@@ -17,8 +17,6 @@ def normalize(a, axis=None):
     -----
     Modifies the input **inplace**.
     """
-    a += np.finfo(float).eps
-
     a_sum = a.sum(axis)
     if axis and a.ndim > 1:
         # Make sure we don't divide by zero.
@@ -35,7 +33,7 @@ def log_normalize(a, axis=None):
 
     Parameters
     ----------
-    a : array_like
+    a : array
         Non-normalized input data.
 
     axis : int