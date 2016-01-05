0ad3c293de0d3f4d7a4864a137270263d7132fea
Sergei Lebedev <superbobry@gmail.com>
2016-01-05 06:15:51 +0300

    TST removed ``test_logsumexp``
      since we don't ship a custom implementation any more

--- hmmlearn/tests/test_utils.py
+++ hmmlearn/tests/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from hmmlearn.utils import normalize, logsumexp
+from hmmlearn.utils import normalize
 
 
 def test_normalize():
@@ -18,14 +18,3 @@ def test_normalize_along_axis():
         assert (A[:, axis] == 0.0).any()
         normalize(A, axis=axis)
         assert np.allclose(A.sum(axis=axis), 1.)
-
-
-def test_logsumexp():
-    x = np.abs(np.random.normal(1e-42, size=100000))
-    logx = np.log(x)
-    assert np.allclose(np.exp(logsumexp(logx)), x.sum())
-
-    X = np.vstack([x, x])
-    logX = np.vstack([logx, logx])
-    assert np.allclose(np.exp(logsumexp(logX, axis=0)), X.sum(axis=0))
-    assert np.allclose(np.exp(logsumexp(logX, axis=1)), X.sum(axis=1))
