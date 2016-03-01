ec985a36c0414e0caf151073ead1df88356c3662
Sergei Lebedev <superbobry@gmail.com>
2016-03-01 11:26:42 +0300

    MAINT added Python3.5 to supported versions

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
 ]
 
 import hmmlearn
