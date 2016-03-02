162f2ccff2ab2ee5b1cd304d7ab599fe63f8c04b
Sergei Lebedev <superbobry@gmail.com>
2016-03-02 14:29:59 +0300

    MAINT run tests in $TEST_DIR

--- continuous_integration/travis/test_script.sh
+++ continuous_integration/travis/test_script.sh
@@ -8,6 +8,11 @@
 
 set -e
 
+# Get into a temp directory to run test from the installed hmmlearn and
+# check if we do not leave artifacts
+mkdir -p $TEST_DIR
+cd $TEST_DIR
+
 python --version
 python -c "import sklearn; print('sklearn %s' % sklearn.__version__)"
 
