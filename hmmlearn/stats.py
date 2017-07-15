ec8661a85e7648e4aa1fe5ccf6646fa9e0e1f8a7
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-07-15 16:11:19 +0100

    Pull log_multivariate_normal_density from sklearn
    Deprecated and scheduled for removal, so make a local copy.

---
+++ hmmlearn/stats.py
@@ -0,0 +1,90 @@
+import numpy as np
+from scipy import linalg
+
+
+def log_multivariate_normal_density(X, means, covars, covariance_type='diag'):
+    """Compute the log probability under a multivariate Gaussian distribution.
+    Parameters
+    ----------
+    X : array_like, shape (n_samples, n_features)
+        List of n_features-dimensional data points. Each row corresponds to a
+        single data point.
+    means : array_like, shape (n_components, n_features)
+        List of n_features-dimensional mean vectors for n_components Gaussians.
+        Each row corresponds to a single mean vector.
+    covars : array_like
+        List of n_components covariance parameters for each Gaussian. The shape
+        depends on `covariance_type`:
+            (n_components, n_features)      if 'spherical',
+            (n_features, n_features)    if 'tied',
+            (n_components, n_features)    if 'diag',
+            (n_components, n_features, n_features) if 'full'
+    covariance_type : string
+        Type of the covariance parameters.  Must be one of
+        'spherical', 'tied', 'diag', 'full'.  Defaults to 'diag'.
+    Returns
+    -------
+    lpr : array_like, shape (n_samples, n_components)
+        Array containing the log probabilities of each data point in
+        X under each of the n_components multivariate Gaussian distributions.
+    """
+    log_multivariate_normal_density_dict = {
+        'spherical': _log_multivariate_normal_density_spherical,
+        'tied': _log_multivariate_normal_density_tied,
+        'diag': _log_multivariate_normal_density_diag,
+        'full': _log_multivariate_normal_density_full}
+    return log_multivariate_normal_density_dict[covariance_type](
+        X, means, covars
+    )
+
+
+def _log_multivariate_normal_density_diag(X, means, covars):
+    """Compute Gaussian log-density at X for a diagonal model."""
+    n_samples, n_dim = X.shape
+    lpr = -0.5 * (n_dim * np.log(2 * np.pi) + np.sum(np.log(covars), 1)
+                  + np.sum((means ** 2) / covars, 1)
+                  - 2 * np.dot(X, (means / covars).T)
+                  + np.dot(X ** 2, (1.0 / covars).T))
+    return lpr
+
+
+def _log_multivariate_normal_density_spherical(X, means, covars):
+    """Compute Gaussian log-density at X for a spherical model."""
+    cv = covars.copy()
+    if covars.ndim == 1:
+        cv = cv[:, np.newaxis]
+    if cv.shape[1] == 1:
+        cv = np.tile(cv, (1, X.shape[-1]))
+    return _log_multivariate_normal_density_diag(X, means, cv)
+
+
+def _log_multivariate_normal_density_tied(X, means, covars):
+    """Compute Gaussian log-density at X for a tied model."""
+    cv = np.tile(covars, (means.shape[0], 1, 1))
+    return _log_multivariate_normal_density_full(X, means, cv)
+
+
+def _log_multivariate_normal_density_full(X, means, covars, min_covar=1.e-7):
+    """Log probability for full covariance matrices."""
+    n_samples, n_dim = X.shape
+    nmix = len(means)
+    log_prob = np.empty((n_samples, nmix))
+    for c, (mu, cv) in enumerate(zip(means, covars)):
+        try:
+            cv_chol = linalg.cholesky(cv, lower=True)
+        except linalg.LinAlgError:
+            # The model is most probably stuck in a component with too
+            # few observations, we need to reinitialize this components
+            try:
+                cv_chol = linalg.cholesky(cv + min_covar * np.eye(n_dim),
+                                          lower=True)
+            except linalg.LinAlgError:
+                raise ValueError("'covars' must be symmetric, "
+                                 "positive-definite")
+
+        cv_log_det = 2 * np.sum(np.log(np.diagonal(cv_chol)))
+        cv_sol = linalg.solve_triangular(cv_chol, (X - mu).T, lower=True).T
+        log_prob[:, c] = - .5 * (np.sum(cv_sol ** 2, axis=1) +
+                                 n_dim * np.log(2 * np.pi) + cv_log_det)
+
+    return log_prob
