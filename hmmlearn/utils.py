hmmlearn/hmmlearn

commit f66a053e7f6b1081f000cd871b73b04ea4a62433

Handle ZERO start/transition probabilities.
  See #93 and #101 for details

hmmlearn/utils.py
@@ -60,3 +60,18 @@ def iter_from_X_lengths(X, lengths):
 
         for i in range(len(lengths)):
             yield start[i], end[i]
+
+def log_mask_zero(prob):
+    """Computes the log of input probabilities masking divide by zero in log.
+
+    Notes
+    -----
+    During EM iteration of the model fitting, very small intermediate
+    start/transition probabilities could be normalized to Zero, causes
+    a RuntimeWarning: divide by zero encountered in log. This function
+    masks this unharmful warning.
+    """
+    prob = np.asarray(prob)
+    with np.errstate(divide='ignore'):
+        _log_prob = np.where(prob > 0, np.log(prob), 0)
+    return _log_prob

hmmlearn/base.py
@@ -11,7 +11,7 @@ from sklearn.utils import check_array, check_random_state
 from sklearn.utils.validation import check_is_fitted
 
 from . import _hmmc
-from .utils import normalize, log_normalize, iter_from_X_lengths
+from .utils import normalize, log_normalize, iter_from_X_lengths, log_mask_zero
 
 
 #: Supported decoder algorithms.
@@ -448,16 +448,16 @@ class _BaseHMM(BaseEstimator):
     def _do_viterbi_pass(self, framelogprob):
         n_samples, n_components = framelogprob.shape
         state_sequence, logprob = _hmmc._viterbi(
-            n_samples, n_components, np.log(self.startprob_),
-            np.log(self.transmat_), framelogprob)
+            n_samples, n_components, log_mask_zero(self.startprob_),
+            log_mask_zero(self.transmat_), framelogprob)
         return logprob, state_sequence
 
     def _do_forward_pass(self, framelogprob):
         n_samples, n_components = framelogprob.shape
         fwdlattice = np.zeros((n_samples, n_components))
         _hmmc._forward(n_samples, n_components,
-                       np.log(self.startprob_),
-                       np.log(self.transmat_),
+                       log_mask_zero(self.startprob_),
+                       log_mask_zero(self.transmat_),
                        framelogprob, fwdlattice)
         return logsumexp(fwdlattice[-1]), fwdlattice
 
@@ -465,8 +465,8 @@ class _BaseHMM(BaseEstimator):
         n_samples, n_components = framelogprob.shape
         bwdlattice = np.zeros((n_samples, n_components))
         _hmmc._backward(n_samples, n_components,
-                        np.log(self.startprob_),
-                        np.log(self.transmat_),
+                        log_mask_zero(self.startprob_),
+                        log_mask_zero(self.transmat_),
                         framelogprob, bwdlattice)
         return bwdlattice
 
@@ -621,7 +621,7 @@ class _BaseHMM(BaseEstimator):
 
             lneta = np.zeros((n_samples - 1, n_components, n_components))
             _hmmc._compute_lneta(n_samples, n_components, fwdlattice,
-                                 np.log(self.transmat_),
+                                 log_mask_zero(self.transmat_),
                                  bwdlattice, framelogprob, lneta)
             stats['trans'] += np.exp(logsumexp(lneta, axis=0))
 

commit 98942a6d4365dd57e0761ae6387bd72774432858

BUG in ``log_mask_zero``: log(0) = -inf
Commit f66a053e7f6b1081f000cd871b73b04ea4a62433 introduced masking of
probabilities that are zero to avoid run-time warning of their logarithm.
It was assumed that the log(x) is 0 for x <= 0. But this corresponds to
x = 1. The right value is -inf for x = 0.

This fix sets log(0) = -inf. Additionally, all values of log(x) for
x < 0 are also set to -inf, which is reasonable considering that x is a
probability and should not be smaller than 0 in the first place.

hmmlearn/utils.py
@@ -76,7 +76,7 @@ def log_mask_zero(a):
     a = np.asarray(a)
     with np.errstate(divide="ignore"):
         a_log = np.log(a)
-        a_log[a <= 0] = 0.0
+        a_log[a <= 0] = -np.inf
         return a_log
 
 