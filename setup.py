38b3cece4a6297e978a204099ae6a0a99555ec01
Antony Lee <anntzer.lee@gmail.com>
2021-04-16 11:45:33 +0200

    Drop remnant of Py2 support.

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@
 #               2018- Antony Lee
 
 
-from io import open
-
 import setuptools
 from setuptools import Extension, find_packages, setup
 from setuptools.command.build_ext import build_ext
