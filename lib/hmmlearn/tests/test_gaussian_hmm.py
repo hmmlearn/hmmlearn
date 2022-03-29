04e135a1fc198bbe3d2ea5c5c44adb78df0263ae
Matthew Danielson <matt.danielson@gmail.com>
2022-03-28 19:15:36 -0700

    Add test case for a GaussianHMM with spherical covariance where n_features is not set correctly

--- lib/hmmlearn/tests/test_gaussian_hmm.py
+++ lib/hmmlearn/tests/test_gaussian_hmm.py
@@ -206,6 +206,21 @@ class TestGaussianHMMWithSphericalCovars(GaussianHMMTestMixin):
     covariance_type = 'spherical'
 
     @pytest.mark.parametrize("implementation", ["scaling", "log"])
+    def test_issue_385(self, implementation):
+        model = hmm.GaussianHMM(n_components=2, covariance_type="spherical")
+        model.startprob_ = np.array([0.6, 0.4])
+        model.transmat_ = np.array([[0.4, 0.6],
+                                                              [0.9, 0.1]])
+        model.means_ = np.array([[3.0],[5.0]])
+        model.covars_ = np.array([[[[4.0]]], [[[3.0]]]])
+
+        # self.n_features not being set correctly
+        covars = model.covars_
+
+        # Make sure covariance is of correct format
+        model.sample(1000)
+
+    @pytest.mark.parametrize("implementation", ["scaling", "log"])
     def test_fit_startprob_and_transmat(self, implementation):
         self.test_fit(implementation, 'st')
 
