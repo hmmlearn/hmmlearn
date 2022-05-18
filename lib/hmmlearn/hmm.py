40b5f2335aed9a9c89ae658da6bd9fc579be7aba
Alex Rockhill <aprockhill@mailbox.org>
2022-05-18 13:06:50 -0700

    [MAINT, MRG] Fix prior for PoissonHMM (#470)
    * fix prior
    
    * simplfy, add comments
    
    * fix out of date comment

--- lib/hmmlearn/hmm.py
+++ lib/hmmlearn/hmm.py
@@ -1191,7 +1191,6 @@ class PoissonHMM(BaseHMM):
 
     def _initialize_sufficient_statistics(self):
         stats = super()._initialize_sufficient_statistics()
-        stats['nsamples'] = 0
         stats['post'] = np.zeros(self.n_components)
         stats['obs'] = np.zeros((self.n_components, self.n_features))
         return stats
@@ -1201,7 +1200,6 @@ class PoissonHMM(BaseHMM):
         super()._accumulate_sufficient_statistics(
             stats, obs, lattice, posteriors, fwdlattice, bwdlattice)
         if 'l' in self.params:
-            stats['nsamples'] += obs.shape[0]
             stats['post'] += posteriors.sum(axis=0)
             stats['obs'] += np.dot(posteriors.T, obs)
 
@@ -1213,7 +1211,10 @@ class PoissonHMM(BaseHMM):
             # section 3.2
             # https://vioshyvo.github.io/Bayesian_inference
             alphas, betas = self.lambdas_prior, self.lambdas_weight
-            n = stats['nsamples']
-            kappas = betas / (betas + n)
+            n = stats['post'].sum()
             y_bar = stats['obs'] / stats['post'][:, None]
-            self.lambdas_ = kappas * (alphas / betas) + (1 - kappas) * y_bar
+            # the same as kappa notation (more intuitive) but avoids divide by
+            # 0, where:
+            # kappas = betas / (betas + n)
+            # self.lambdas_ = kappas * (alphas / betas) + (1 - kappas) * y_bar
+            self.lambdas_ = (alphas + n * y_bar) / (betas + n)
